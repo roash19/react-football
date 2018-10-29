e890630f0d8a4c6be60ca183e861d9e1642d4c0d
roash19 <roash19@mail.ru>
2018-10-29 19:15:06 +0300

    cards

---
+++ src/Components/Meetplayers/Cards.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { easePolyInOut } from 'd3-ease';
+import Animate from 'react-move/Animate';
+
+class HomeCards extends Component {
+  state = {
+    show: this.props.show
+  };
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default HomeCards;
